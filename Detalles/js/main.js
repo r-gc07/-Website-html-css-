45ed9d65092ce6ec7c7d661e3300be11f48f3094
r-gc07 <rgorcas0709@g.educaand.es>
2025-12-11 19:56:49 +0100

    Agregación (Scroll)

--- Detalles/js/main.js
+++ Detalles/js/main.js
@@ -6,18 +6,44 @@
 
 /*=============== ADD SHADOW HEADER ===============*/
 const shadowHeader = () => {
-    const header = document.getElementById('header')   
+    const header = document.getElementById('header')
     //Add a class if the bottom offset is greater than 50 viewport height
-    this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+        : header.classList.remove('shadow-header')
 }
-window.addEventListener('scroll', shadowHeader) 
+window.addEventListener('scroll', shadowHeader)
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+     if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        sectionsClass.classList.add('active-link')
+     }else{
+        sectionsClass.classList.remove('active-link')
+     }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
